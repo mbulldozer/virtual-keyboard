eea5126a7bf34e636dc15d8f0ea820c6f7c294f5
Aliaksandr Pauliukavets <al.pavlyukovets@yandex.ru>
2022-05-04 00:28:52 +0300

    feat: add autofocus for textarea

--- js/keyboard.js
+++ js/keyboard.js
@@ -9,12 +9,16 @@ TEXTAREA.setAttribute('rows', 10);
 KEYBOARD.classList.add('keyboard');
 document.body.append(CONTAINER);
 CONTAINER.append(TEXTAREA, KEYBOARD);
+TEXTAREA.focus();
 
 KEYS.forEach(key => {
-  console.log(KEYBOARD);
   let keyNode = document.createElement('div');
-  keyNode.innerText=key.eng.toUpperCase();
+  keyNode.innerHTML=key.classes.includes("letter")?key.eng.toUpperCase():key.eng;
   key.classes.forEach(el => keyNode.classList.add(el));
   KEYBOARD.append(keyNode);
+});
+
+TEXTAREA.addEventListener("blur", () => {
+    TEXTAREA.focus();
 });
 
