2ed7fc00aafc2209a31e14a46ee14c8ddb6032fc
Aliaksandr Pauliukavets <al.pavlyukovets@yandex.ru>
2022-05-05 12:10:13 +0300

    fix: use export for keys data, fix eslint errors

--- js/keyboard.js
+++ js/keyboard.js
@@ -1,3 +1,5 @@
+import KEYS from './keys';
+
 const CONTAINER = document.createElement('div');
 const TEXTAREA = document.createElement('textarea');
 const KEYBOARD = document.createElement('div');
@@ -11,27 +13,26 @@ document.body.append(CONTAINER);
 CONTAINER.append(TEXTAREA, KEYBOARD);
 TEXTAREA.focus();
 
-KEYS.forEach(key => {
-    let keyNode = document.createElement('button');
-    keyNode.setAttribute('type', 'button');
-    keyNode.innerHTML = key.classes.includes("letter") ? key.eng.toUpperCase() : key.eng;
-    key.classes.forEach(el => keyNode.classList.add(el));
-    KEYBOARD.append(keyNode);
+KEYS.forEach((key) => {
+  const keyNode = document.createElement('button');
+  keyNode.setAttribute('type', 'button');
+  keyNode.innerHTML = key.classes.includes('letter') ? key.eng.toUpperCase() : key.eng;
+  key.classes.forEach((el) => keyNode.classList.add(el));
+  KEYBOARD.append(keyNode);
 });
 
-window.addEventListener("mousedown", e => {
-    if (e.target.tagName === 'BUTTON') {
-      e.target.classList.add('pressed');
-    }
+window.addEventListener('mousedown', (e) => {
+  if (e.target.tagName === 'BUTTON') {
+    e.target.classList.add('pressed');
+  }
 });
 
-window.addEventListener("mouseup", e => {
-    if (e.target.tagName === 'BUTTON') {
-        e.target.classList.remove('pressed');
-    }
+window.addEventListener('mouseup', (e) => {
+  if (e.target.tagName === 'BUTTON') {
+    e.target.classList.remove('pressed');
+  }
 });
 
-TEXTAREA.addEventListener("blur", () => {
-    TEXTAREA.focus();
+TEXTAREA.addEventListener('blur', () => {
+  TEXTAREA.focus();
 });
-
