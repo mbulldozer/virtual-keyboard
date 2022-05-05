7cefc8ed19b7d7c731eea24d13d343a91110c2d5
Aliaksandr Pauliukavets <al.pavlyukovets@yandex.ru>
2022-05-05 11:55:42 +0300

    feat: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "airbnb-base",
+    "parserOptions": {
+        "ecmaVersion": 2021,
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+};
