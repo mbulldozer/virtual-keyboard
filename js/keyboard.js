2d70d51ead11e8cc56255923d6369a72a0de1abc
Aliaksandr Pauliukavets <al.pavlyukovets@yandex.ru>
2022-05-05 11:27:24 +0300

    feat: add keypress animation

--- js/keyboard.js
+++ js/keyboard.js
@@ -4,7 +4,7 @@ const KEYBOARD = document.createElement('div');
 
 CONTAINER.classList.add('container');
 TEXTAREA.classList.add('textarea');
-TEXTAREA.setAttribute('cols',30);
+TEXTAREA.setAttribute('cols', 30);
 TEXTAREA.setAttribute('rows', 10);
 KEYBOARD.classList.add('keyboard');
 document.body.append(CONTAINER);
@@ -12,10 +12,23 @@ CONTAINER.append(TEXTAREA, KEYBOARD);
 TEXTAREA.focus();
 
 KEYS.forEach(key => {
-  let keyNode = document.createElement('div');
-  keyNode.innerHTML=key.classes.includes("letter")?key.eng.toUpperCase():key.eng;
-  key.classes.forEach(el => keyNode.classList.add(el));
-  KEYBOARD.append(keyNode);
+    let keyNode = document.createElement('button');
+    keyNode.setAttribute('type', 'button');
+    keyNode.innerHTML = key.classes.includes("letter") ? key.eng.toUpperCase() : key.eng;
+    key.classes.forEach(el => keyNode.classList.add(el));
+    KEYBOARD.append(keyNode);
+});
+
+window.addEventListener("mousedown", e => {
+    if (e.target.tagName === 'BUTTON') {
+      e.target.classList.add('pressed');
+    }
+});
+
+window.addEventListener("mouseup", e => {
+    if (e.target.tagName === 'BUTTON') {
+        e.target.classList.remove('pressed');
+    }
 });
 
 TEXTAREA.addEventListener("blur", () => {
