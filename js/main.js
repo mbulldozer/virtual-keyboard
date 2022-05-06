c9a9e28ce9e5826cb42aee75f9bbd4f1fdab4f49
Aliaksandr Pauliukavets <al.pavlyukovets@yandex.by>
2022-05-06 11:40:23 +0300

    fix: make mouseup event work correctly

--- js/main.js
+++ js/main.js
@@ -1,16 +1,19 @@
 import Keyboard from './keyboard.js';
 
 const KEYBOARD = new Keyboard();
+let pressed = null;
 
 window.addEventListener('mousedown', (e) => {
   if (e.target.tagName === 'BUTTON') {
     KEYBOARD.press(e.target.dataset.code);
+    pressed = e.target.dataset.code;
   }
 });
 
-window.addEventListener('mouseup', (e) => {
-  if (e.target.tagName === 'BUTTON') {
-    KEYBOARD.unpress(e.target.dataset.code);
+window.addEventListener('mouseup', () => {
+  if (pressed) {
+    KEYBOARD.unpress(pressed);
+    pressed = null;
   }
 });
 
